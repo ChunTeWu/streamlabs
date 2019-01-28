6fadd80119277bed105cc5a23ce2db6c672d29b7
ChunTeWu <davidpypy@gmail.com>
2019-01-27 21:24:57 -0800

    menu css update and munu function updated

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,11 +1,25 @@
 (function($){
 	'use strict';
-	var $window = $(window);
+	// toggle class when menu icon is clicked
+	$(".menu__mobile__icon").on('click', function(){
+		$('.menu__link__container').toggleClass('menu__link__container--mobile-active');
+		if($('.menu').hasClass('menu--bg-active') && $window.scrollTop() > 100){
+			return;
+		}else{
+			$('.menu').addClass('menu--bg-active');
+		}
 
-	$(".menu__mobile__icon").on('click', function(){//expend the menu list
-        $('.menu__link__container').toggleClass('menu__link__container--mobile-active');
-        $('.menu').toggleClass('menu--bg-active');
 	});
+	// add background when window scrolls down
+	$(window).on('scroll', function(){
+		if($(window).scrollTop() > 100){
+			$('.menu').addClass('menu--bg-active');
+		}else{
+			$('.menu').removeClass('menu--bg-active');
+			
+		}
+	});
+
 
 })(jQuery);
 
